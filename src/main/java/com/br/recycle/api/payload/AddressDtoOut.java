695f905bc0fd38fee8891c71f4269fc2a5ca6b2c
CaioBastosImpacta <caio.bastos@aluno.faculdadeimpacta.com.br>
2021-07-14 19:36:29 -0300

    correcao de validacao

--- src/main/java/com/br/recycle/api/payload/AddressDtoOut.java
+++ src/main/java/com/br/recycle/api/payload/AddressDtoOut.java
@@ -1,5 +1,7 @@
 package com.br.recycle.api.payload;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
@@ -10,6 +12,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@JsonInclude(Include.NON_EMPTY)
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class AddressDtoOut {
 
