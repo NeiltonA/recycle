74825703e495b69e793c30834d8f20e80ad17bae
CaioBastosImpacta <caio.bastos@aluno.faculdadeimpacta.com.br>
2021-07-20 22:15:14 -0300

    correcao burrada 2

--- src/main/java/com/br/recycle/api/service/RateService.java
+++ src/main/java/com/br/recycle/api/service/RateService.java
@@ -48,7 +48,7 @@ public class RateService {
 			response = rateRepository.findByDonationCooperativeId(cooperative);
 			return validateEmpty(response);
 		} else if (Objects.nonNull(user)) {
-			response = rateRepository.findByDonationCooperativeUserId(user);
+			response = rateRepository.findByDonationGiverUserId(user);
 			return validateEmpty(response);
 		} else {
 			response = rateRepository.findAll();
